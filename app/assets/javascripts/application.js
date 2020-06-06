eb6bdf631ae2bbcf26e302be15182cbe859e1731
Shabin Kv <shabinkv3@gmail.com>
2020-06-06 13:44:02 +0530

    mess cut correction

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -412,7 +412,8 @@ async function showToast(message) {
 
 
 
-function viewOption(evt, option) {
+function viewOptionMess(evt, option) {
+  document.getElementById('mess_cut_data').style.display='none';
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
@@ -432,6 +433,20 @@ function viewOption(evt, option) {
   {
     getStaffs();
   }
+}
+
+function viewOptionStudent(evt, option) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("side_tabs");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace("active", "");
+  }
+  evt.currentTarget.className += " active";
+  document.getElementById(option).style.display = "block";  
 }
 
 
